6c63372acdf5f0e84b5e9f1d219614125262a40a
Alessandro D'Amico <alessandro.damico2@stud.unifi.it>
2019-06-11 22:35:49 +0200

    Added Matplotlib GUI
    GUI displaying the two sorting algs results

--- InsertionVsMergeBenchmark.py
+++ InsertionVsMergeBenchmark.py
@@ -1,16 +1,17 @@
 import random
 import math
+from timeit import default_timer as timer
 import numpy as np
 import matplotlib.pyplot as plt
-from timeit import default_timer as timer
 
+timeLimit=30
 #  Insertion Sort  #
-
 def Insertion_sort(A):
  start = timer()
  for i in range(1, len(A)):
-  if timer()-start > 300:
-     return print("Tempo Insertion Sort: Il tempo supera i 5 min")
+  if timer()-start > timeLimit:
+     print("Tempo Limite Superato")
+     return timeLimit
   else:
      key = A[i]
      j = i-1
@@ -19,7 +20,8 @@ def Insertion_sort(A):
         j = j-1
      A[j+1] = key
  end = timer()
- return print("Tempo Insertion Sort", end-start)
+ print(end-start)
+ return end-start
 
 #   Merge Sort   #
 
@@ -61,7 +63,8 @@ def random_vect(B):
 #          dimensione del vettore dei numeri da ordinare
 
 numbervect = [10,100,1000,10000,100000,1000000]
-
+insertionSortGraph = []
+mergeSortGraph = []
 
 
 for j in numbervect:
@@ -72,18 +75,22 @@ for j in numbervect:
 
   #    Esecuzione Insertion Sort   #
 
-  Insertion_sort(A)
+  print("Tempo Insertion Sort")
+  insertionSortGraph.append(Insertion_sort(A))
 
-  #    Esecuzione Merge Sort   (timer messo fuori per comodita')  #
+  #   Esecuzione Merge Sort   (timer messo fuori per comodita')  #
 
   start = timer()
   Merge_sort(B, 0, len(B)-1)
   end = timer()
+
+  mergeSortGraph.append(end-start)
+
   print("Tempo Merge Sort ", end - start, "\n", "_____Ordinamento terminato_____", "\n")
 
-  # Plot
 
-x = np.arange(0, 3*np.pi, 0.1)
-y = np.sin(x)
-plt.plot(x, y)
+
+plt.plot(numbervect,mergeSortGraph)
+plt.plot(numbervect, insertionSortGraph)
 plt.show()
+
